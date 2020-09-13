ebf7556462aee03036a0e1a60152ccd90f3dd38e
Allen Carvallo <53426022+allencarvallo@users.noreply.github.com>
2020-09-13 19:32:45 +0530

    Add files via upload

---
+++ Reaction Tester/script.js
@@ -0,0 +1,47 @@
+var start = new Date().getTime();
+
+            function getRandomColor() {
+                var letters = "0123456789ABCDEF".split('');
+                var color = "#";
+                for (var i = 0; i < 6; i++) {
+                    color += letters[ Math.floor(Math.random() * 16) ];
+                }
+                return color;
+            }
+            
+            function makeShapeApppear() {      
+                var top = Math.random() * 500;
+                var left = Math.random() * 1000;
+                var width = (Math.random() * 200) + 100;        // to get a minimum width of 100
+                var color = getRandomColor();
+
+                if( Math.random() > 0.5) {
+                    document.getElementById("shape").style.borderRadius = "50%";
+                } else {
+                    document.getElementById("shape").style.borderRadius = "0";
+                }
+
+                document.getElementById("shape").style.backgroundColor = color;
+                document.getElementById("shape").style.width = width + "px";
+                document.getElementById("shape").style.height = width + "px";
+                document.getElementById("shape").style.top = top + "px";
+                document.getElementById("shape").style.left = left + "px";
+                document.getElementById("shape").style.display = "block";
+                start = new Date().getTime();
+            }
+
+            function appearAfterDelay() {
+                setTimeout(makeShapeApppear, Math.random() * 2000);
+            }
+
+            appearAfterDelay();
+            
+            document.getElementById("shape").onclick = function() {
+
+                document.getElementById("shape").style.display = "none";
+                var end = new Date().getTime();
+                var timeTaken = (end - start) / 1000;
+                document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+                appearAfterDelay();
+                
+            }
